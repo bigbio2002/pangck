1bf66b91fb8967285b8ed9000bcf651c198b8795
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2024-01-25 11:00:52 -0500

    start writing main workhorse function

---
+++ pangcheck.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int pangramCheck(string &sentence)
+{
+	//longest (sensible) pangram I could find on Google was 51 characters. Perhaps a creative linguist coukd make one longer, but not too much so.
+	char sentenceChars[64];
+}
